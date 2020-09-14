fcd842242fb40bda10c047a83de4be39345e1790
Vladimir Kalugin <kaluginbc@gmail.com>
2020-09-14 20:13:32 +0700

    made layer corner radius.

--- Casco/Controllers/ViewController.swift
+++ Casco/Controllers/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var startButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = startButton.frame.width / 6
     }
 
 
